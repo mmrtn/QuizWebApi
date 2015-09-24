7872c666a877e8bf1882d4b77a7afa4e83b34fe5
Martin Meltsas <martin.meltsas@khk.ee>
2015-09-24 23:44:32 +0300

    Create Class for Database [Delivers #104132650]

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+// "localhost", "root", "", "hot100_billboard"
+define('DATABASE_HOSTNAME', 'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_NAME', 'musiq_quiz');
+define('WEBSITE_EMAIL', '');
