b8292d951978777409470a2863a354719b2cc4b1
Martin Meltsas <martin.meltsas@khk.ee>
2015-09-25 00:09:54 +0300

    Create Class for Database [Delivers #104132650]

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-// "localhost", "root", "", "hot100_billboard"
+//! "localhost", "root", "", "hot100_billboard"
 define('DATABASE_HOSTNAME', 'localhost');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
