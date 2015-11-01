0415c7b66254eaf4b66ec04aef20c51b4541c727
Martin Meltsas <martin.meltsas@khk.ee>
2015-11-01 03:03:14 +0200

    upload API to live server and test it! [Delivers #104943284]

---
+++ config_sample.php
@@ -0,0 +1,7 @@
+<?php
+//! "localhost", "root", "", "hot100_billboard"
+define('DATABASE_HOSTNAME', 'localhost');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_NAME', 'music_quiz');
+define('WEBSITE_EMAIL', '');
