337a6a325e96d16960e6631e70373fbf8d0ec704
Martin Meltsas <martin.meltsas@khk.ee>
2015-11-18 03:37:41 +0200

    Update Quiz test creation [Delivers #108333142]

--- Classes/Quiz.php
+++ Classes/Quiz.php
@@ -22,27 +22,38 @@ class Quiz
      */
     private function create_test($year_from, $year_to, $nr_of_questions)
     {
-        $nr_of_questions *= 6;
+        $nr_of_variants = 6 * $nr_of_questions * 2;
+
         $test = array();
+        $artists = array();
         $test_variant = array();
         $db_conn = new Database();
 
         // example: SELECT * FROM hot100_billboard WHERE year>=1960 and year<=1965 ORDER BY RAND() LIMIT 50
 
-        $sql = "SELECT * FROM hot100_billboard WHERE year>=$year_from and year<=$year_to ORDER BY RAND() LIMIT $nr_of_questions";
+        $sql = "SELECT * FROM hot100_billboard WHERE year>=$year_from and year<=$year_to ORDER BY RAND() LIMIT $nr_of_variants";
 
         $query = $db_conn->db_query($sql);
 
         foreach ($query as $i) {
 
-            $test[] = $i;
+            if (!in_array($i['artist'], $artists)) {
+                $artists[] = $i['artist'];
+                $test[] = $i;
+            }
+
             if (count($test) === 6) {
-                $test_variant[] = new Variants($test);
+
+                if (count($test_variant) < $nr_of_questions) {
+                    $test_variant[] = new Variants($test);
+                }
                 $test = array();
+                $artists = array();
             }
         }
 
         $db_conn->close_conn();
+
         return $test_variant;
     }
 
